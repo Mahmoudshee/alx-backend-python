23614a1496870e039d40015e9c03387d776d955d
Mahmoud shee <mahmoudshee8914@gmail.com>
2023-07-28 04:53:57 -0700

    kenty

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,140 +1,148 @@
 #!/usr/bin/env python3
-""" Test cases for utils module """
+"""
+Test Module for tasks in 0x03-Unittests_and_integration_tests
+"""
 
 import unittest
-from unittest.mock import patch, Mock
+from fixtures import GITHUB_API_DATA
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized, parameterized_class
+from typing import Any
 from utils import access_nested_map, get_json, memoize
 from client import GithubOrgClient
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    # Task 0
+    """
+    TestAccessNestedMap Class
+    """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Test access_nested_map function """
+        """
+        Test access_nested_map function
+        """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
 
-    # Task 1
     @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
     ])
     def test_access_nested_map_exception(self, nested_map, path, expected):
-        """ Test access_nested_map function with exceptions """
+        """
+        Test access_nested_map function for exception
+        """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    # Task 2
+    """
+    TestGetJson Class
+    """
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, test_url, test_payload):
-        """ Test get_json function """
-        with patch('requests.get') as mock_get:
-            mock_get.return_value.json.return_value = test_payload
-            result = get_json(test_url)
-            self.assertEqual(result, test_payload)
-            mock_get.assert_called_once_with(test_url)
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test get_json function
+        """
+        mock_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
 
 
 class TestMemoize(unittest.TestCase):
-    # Task 3
+    """
+    TestMemoize Class
+    """
+
     class TestClass:
+        """
+        TestClass for memoization
+        """
+
         def a_method(self):
+            """
+            Test method
+            """
             return 42
 
         @memoize
         def a_property(self):
+            """
+            Test property
+            """
             return self.a_method()
 
-    def test_memoize(self):
-        """ Test memoize decorator """
-        with patch.object(
-            self.TestClass, 'a_method', return_value=42
-        ) as mock_method:
-            instance = self.TestClass()
-            result = instance.a_property
-            result2 = instance.a_property
-            self.assertEqual(result, 42)
-            self.assertEqual(result2, 42)
-            mock_method.assert_called_once()
+    @patch.object(TestClass, 'a_method')
+    def test_memoize(self, mock_a_method):
+        """
+        Test memoize decorator
+        """
+        instance = self.TestClass()
+        instance.a_property()
+        instance.a_property()
+        mock_a_method.assert_called_once()
 
 
 @parameterized_class([
-    {
-        "org_payload": payload["org_payload"],
-        "repos_payload": payload["repos_payload"],
-        "expected_repos": payload["expected_repos"],
-        "apache2_repos": payload["apache2_repos"]
-    }
-    for payload in [
-        {
-            "org_payload": {"repos_url": "http://example.com/repos"},
-            "repos_payload": {"name": "test_repo"},
-            "expected_repos": ["test_repo"],
-            "apache2_repos": []
-        }
-    ]
+    {"org_payload": org_payload, "repos_payload": repos_payload,
+     "expected_repos": expected_repos, "apache2_repos": apache2_repos}
+    for org_payload, repos_payload, expected_repos, apache2_repos in
+    fixtures.GITHUB_API_DATA.values()
 ])
-class TestGithubOrgClient(unittest.TestCase):
-    # Task 4
-    @patch('client.get_json')
-    def test_org(self, mock_get):
-        """ Test org method of GithubOrgClient """
-        mock_get.return_value = self.org_payload
-        client = GithubOrgClient("test_org")
-        self.assertEqual(client.org, self.org_payload)
-        mock_get.assert_called_once_with("http://example.com/repos")
-
-    # Task 7
-    @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False),
-    ])
-    def test_has_license(self, repo, license_key, expected):
-        """ Test has_license method of GithubOrgClient """
-        client = GithubOrgClient("test_org")
-        result = client.has_license(repo, license_key)
-        self.assertEqual(result, expected)
-
-
 class TestIntegrationGithubOrgClient(unittest.TestCase):
-    # Task 8
+    """
+    TestIntegrationGithubOrgClient Class
+    """
+
     @classmethod
     def setUpClass(cls):
-        """ Set up test class """
-        patcher = patch('client.get_json')
-        cls.mock_get = patcher.start()
-        cls.mock_get.return_value = [{"name": "test_repo"}]
+        """
+        Set up class method
+        """
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
 
     @classmethod
     def tearDownClass(cls):
-        """ Tear down test class """
-        cls.mock_get.stop()
+        """
+        Tear down class method
+        """
+        cls.get_patcher.stop()
 
     def test_public_repos(self):
-        """ Test public_repos method of GithubOrgClient """
-        client = GithubOrgClient("test_org")
+        """
+        Test public_repos method
+        """
+        client = GithubOrgClient("test")
+        self.mock_get.return_value.json.side_effect = [
+            self.org_payload, self.repos_payload
+        ]
         repos = client.public_repos()
-        self.assertEqual(repos, ["test_repo"])
+        self.assertEqual(repos, self.expected_repos)
 
-    # Task 9
     def test_public_repos_with_license(self):
-        """ Test public_repos method of GithubOrgClient with license """
-        client = GithubOrgClient("test_org")
-        with patch.object(client, 'has_license', return_value=True):
-            repos = client.public_repos("apache-2.0")
-            self.assertEqual(repos, ["test_repo"])
+        """
+        Test public_repos method with license parameter
+        """
+        client = GithubOrgClient("test")
+        self.mock_get.return_value.json.side_effect = [
+            self.org_payload, self.repos_payload
+        ]
+        repos = client.public_repos("apache-2.0")
+        self.assertEqual(repos, self.apache2_repos)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,37 +1,75 @@
 #!/usr/bin/env python3
-""" Utility functions for the tasks """
-
+"""Generic utilities for github org client.
+"""
 import requests
 from functools import wraps
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
 
 
-def access_nested_map(nested_map, path):
-    """ Access nested items in a dictionary """
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access nested map with key path.
+    Parameters
+    ----------
+    nested_map: Mapping
+        A nested map
+    path: Sequence
+        a sequence of key representing a path to the value
+    Example
+    -------
+    >>> nested_map = {"a": {"b": {"c": 1}}}
+    >>> access_nested_map(nested_map, ["a", "b", "c"])
+    1
+    """
     for key in path:
-        if key in nested_map:
-            nested_map = nested_map[key]
-        else:
-            return None
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
+        nested_map = nested_map[key]
+
     return nested_map
 
 
-def get_json(url):
-    """ Send a GET request and parse JSON response """
+def get_json(url: str) -> Dict:
+    """Get JSON from remote URL.
+    """
     response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    return None
+    return response.json()
 
 
-def memoize(func):
-    """ Memoize decorator to cache function results """
-    cache = {}
+def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
+    """
+    attr_name = "_{}".format(fn.__name__)
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        key = args + tuple(sorted(kwargs.items()))
-        if key not in cache:
-            cache[key] = func(*args, **kwargs)
-        return cache[key]
+    @wraps(fn)
+    def memoized(self):
+        """"memoized wraps"""
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
 
-    return wrapper
+    return property(memoized)
