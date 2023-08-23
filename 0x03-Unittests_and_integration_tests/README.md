493120ca7360a94e9514f8d18e5b8c6b326337a8
Mahmoudshee <mahmoudshee8914@gmail.com>
2023-08-23 20:08:12 +0000

    kenty

--- 0x03-Unittests_and_integration_tests/README.md
+++
@@ -1,2 +0,0 @@
-my readme file for 0x03-Unittests_and_integration_tests
-this task is working on unit testing and integration testing
--- 0x03-Unittests_and_integration_tests/fixtures.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" Test fixtures for utils module """
-
-# Test fixtures for Task 2
-TEST_PAYLOAD = [
-    {"org_payload": {"repos_url": "http://example.com/repos"}, "repos_payload": {"name": "test_repo"}, "expected_repos": ["test_repo"], "apache2_repos": []}
-]
